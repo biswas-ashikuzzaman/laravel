c7a5bced4637c84d16d64e6be2ea207dba1a7668
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-10-23 23:58:53 +0600

    update

--- banking_management_system_project/banking_management_system/routes/web.php
+++ banking_management_system_project/banking_management_system/routes/web.php
@@ -3,8 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('ashik');
 });
 Route::get('/about', function(){
     return "This is the about page.";
+});
+Route::get('/home', function(){
+    return view('Home');
+});
+Route::get('/about', function(){
+    return view('About');
 });
