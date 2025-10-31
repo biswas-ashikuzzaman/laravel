be43d0b59a8ee287a9df0b7fbfc3bb7eb617d3e9
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-10-31 11:54:31 +0600

    portfolio done

--- laraeshoap/routes/web.php
+++ laraeshoap/routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 // Dashboard route using UserController
