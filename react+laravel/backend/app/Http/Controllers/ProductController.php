322fc2d3fec2105ebb1ad14025658731b3e294c3
Ashikuzzaman Biswas <biswasashikuzzaman@gmail.com>
2025-11-16 09:49:23 +0600

    Create ProductController.php

---
+++ react+laravel/backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+        return $product;
+    }
+
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return $product;
+    }
+
+    public function destroy($id)
+    {
+        return Product::destroy($id);
+    }
+}
